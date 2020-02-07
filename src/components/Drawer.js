8f3ba9ca7e771a48c2eae15155a7188df0ab5b29
Jake Jenne <jake.jenne@nuvi.com>
2020-02-07 13:23:13 -0700

    transitions

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 
 class Drawer extends React.Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
     onCancel: PropTypes.func.isRequired,
+    display: PropTypes.bool.isRequired,
   }
   render() {
     return (
       <React.Fragment>
-        <div className="shadow" onClick={ this.props.onCancel }></div>
-        <div className="drawer">
-          { this.props.children }
-        </div>
+        <CSSTransition in={ this.props.display } timeout={500} classNames="fade" unmountOnExit>
+          <div className="shadow" onClick={ this.props.onCancel }></div>
+        </CSSTransition>
+        <CSSTransition in={ this.props.display } timeout={500} classNames="slide" unmountOnExit>
+          <div className="drawer">
+            { this.props.children }
+          </div>
+        </CSSTransition>
       </React.Fragment>
     );
   }
